f808d93f276da5f1f2affc9753681da1af446427
Teodoro <doromenendez191-cloud@users.noreply.github.com>
2026-06-05 23:16:04 +0200

    Actualización final de Tarea 2: archivos corregidos

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,16 +1,10 @@
+import requests
 import json
-from watson_nlp import EmotionPredictor
 
-def emotion_detector(text):
-    model = EmotionPredictor.from_pretrained("emotion_aggregated-workflow_lang_en_stock")
-    prediction = model.predict(text)
-    emotions = prediction.get_emotions()
-    dominant_emotion = max(emotions, key=emotions.get)
-    return {
-        "anger": emotions.get("anger"),
-        "disgust": emotions.get("disgust"),
-        "fear": emotions.get("fear"),
-        "joy": emotions.get("joy"),
-        "sadness": emotions.get("sadness"),
-        "dominant_emotion": dominant_emotion
-    }
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()['text']
